4781b93f3fd166622487cfe8bf016e084eaf114b
carnuare <carnuare@gmail.com>
2023-01-19 13:19:43 +0100

    Add sla & pricing

--- src/common/footer.js
+++ src/common/footer.js
@@ -2,9 +2,9 @@ function Footer() {
     return (
         <footer className="text-center">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">Agreements</a></li>
-                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">Pricing</a></li>
-                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">Contact us</a></li>
+                <li className="nav-item"><a href="https://fastmusik-fastmusik-marmolpen3.cloud.okteto.net/customer-agreement" target="_blank" className="nav-link px-2 text-muted">Agreements</a></li>
+                <li className="nav-item"><a href="https://fastmusik-fastmusik-marmolpen3.cloud.okteto.net/pricing" target="_blank" className="nav-link px-2 text-muted">Pricing</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Contact us</a></li>
             </ul>
             <p>© 2022 FastMusik app</p>
         </footer>
